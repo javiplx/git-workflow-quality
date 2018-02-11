542bf98c09a9b1476c0ac086adc4f240304ce46b
Javier Palacios <javiplx@gmail.com>
2018-02-11 21:40:19 +0100

    Add python setuptools file

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name = 'git-workflow-quality',
+    version = '0.9',
+    description = 'Quality assessment of git workflows & repositories',
+    author = 'Javier Palacios',
+    author_email = 'javiplx@gmail.com',
+    license = 'GPLv2',
+    url = 'http://github.com/javiplx/git-workflow-quality',
+    scripts = [ 'git-workflow-quality' ],
+    packages = [ 'git_workflow_quality' ]
+    )
+
+
