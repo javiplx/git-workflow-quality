c2358b45b99fbcb9c59a8295c2e85935821757eb
Javier Palacios <javiplx@gmail.com>
2018-02-27 22:43:35 +0100

    Add test case for javiplx/git-workflow-quality#7

--- do_test.py
+++ do_test.py
@@ -7,6 +7,12 @@ import os
 os.chdir('tests/test1.git')
 repo = git_workflow_quality.repository()
 
+c = repo.commits['b8285f2f001cfad3eacfb5c642623822f5fb60a1']
+if c.branch == 'master' :
+    print "OK - %s" % c
+else :
+    print "ERROR - %s" % c
+
 c = repo.commits['3dec0a264f6b28901cf2151e70d5691b696a4e9d']
 if c.forks and not c.child :
     print "ERROR - %s" % c
