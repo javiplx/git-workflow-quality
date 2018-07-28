ba0688610f004be3eb4fa472b67255855a1ccc78
Javier Palacios <javiplx@gmail.com>
2018-07-28 16:02:28 +0200

    Improve handling of tests output

--- do_test.py
+++ do_test.py
@@ -4,6 +4,20 @@ import git_workflow_quality
 
 import os
 
+def ok( msg , fd=os.sys.stdout ) :
+    if os.environ['TERM'] == 'cygwin':
+        fd.write( "OK - %s\n" % msg )
+    else :
+        fd.write( "\033[1;32mOK\033[1;m - %s\n" % msg )
+    return 0
+
+def fail( msg , fd=os.sys.stdout ) :
+    if os.environ['TERM'] == 'cygwin':
+        fd.write( "ERROR - %s\n" % msg )
+    else :
+        fd.write( "\033[1;31mERROR\033[1;m - %s\n" % msg )
+    return 1
+
 ret = 0
 
 os.chdir('tests/test1.git')
@@ -11,17 +25,15 @@ repo = git_workflow_quality.Repository()
 
 c = repo['b8285f2f001cfad3eacfb5c642623822f5fb60a1']
 if c.branch.name == 'master' :
-    print "\033[1;32mOK\033[1;m - %s owned by master" % c.sha
+    ok( "%s owned by master" % c.sha )
 else :
-    print "\033[1;31mERROR\033[1;m - %s" % ( c.sha , c.branch.name )
-    ret += 1
+    ret += fail( "%s on branch" % ( c.sha , c.branch.name ) )
 
 c = repo['3dec0a264f6b28901cf2151e70d5691b696a4e9d']
 if c.forks and not c.child :
-    print "\033[1;31mERROR\033[1;m - %s got no child assigned" % c.sha
-    ret += 1
+    ret += fail( "%s got no child assigned" % c.sha )
 else :
-    print "\033[1;32mOK\033[1;m - child assigned to %s" % c.sha
+    ret += ok( "child assigned to %s" % c.sha )
 
 os.chdir('../..')
 
@@ -31,11 +43,9 @@ def test_event ( event ) :
     repo = git_workflow_quality.Repository()
     count = repo.event_list()[0][event]
     if count == 1 :
-        print "\033[1;32mOK\033[1;m - 1 %s found" % event
-	ret = 0
+        ret = ok( "1 %s found" % event )
     else :
-        print "\033[1;31mERROR\033[1;m - %d %s found" % ( count , event )
-	ret = 1
+        ret = fail( "%d %s found" % ( count , event ) )
     os.chdir('../..')
     return ret
 
