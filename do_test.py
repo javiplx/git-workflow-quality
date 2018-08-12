2a0aa653d4d757fc36871b2125fd02e2b2790c28
Javier Palacios <javiplx@gmail.com>
2018-08-12 13:02:36 +0200

    Add test for initial commit discard

--- do_test.py
+++ do_test.py
@@ -5,19 +5,25 @@ import git_workflow_quality
 import os
 
 os.chdir('tests/test1.git')
-repo = git_workflow_quality.repository()
+repo = git_workflow_quality.Repository()
 
-c = repo.commits['b8285f2f001cfad3eacfb5c642623822f5fb60a1']
-if c.branch == 'master' :
+sha = 'b8285f2f001cfad3eacfb5c642623822f5fb60a1'
+if sha in repo :
+    print "OK - Initial commit included"
+else :
+    print "ERROR - commit %s discarded" % sha
+
+sha = 'b8285f2f001cfad3eacfb5c642623822f5fb60a1'
+c = repo.get(sha)
+if c and c.branch.name == 'master' :
     print "OK - %s" % c
 else :
     print "ERROR - %s" % c
 
-c = repo.commits['3dec0a264f6b28901cf2151e70d5691b696a4e9d']
-if c.forks and not c.child :
+sha = '3dec0a264f6b28901cf2151e70d5691b696a4e9d'
+c = repo.get(sha)
+if not c or ( c.forks and not c.child  ):
     print "ERROR - %s" % c
 else :
     print "OK - %s" % c
-
-git_workflow_quality.gitgraphjs.graph( repo , 'date' )
 
